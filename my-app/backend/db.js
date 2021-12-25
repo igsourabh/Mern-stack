07933ea61b541f9f0cdaf189970388153912bc81
igsourabh <sourabhvaish007@gmail.com>
2021-12-25 13:11:31 +0530

    install mongoose and nodemon

---
+++ my-app/backend/db.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const mongoURI =
+  "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
+
+const connectToMongo = () => {
+  mongoose.connect(mongoURI, () => {
+    console.log("connected to mongoose to sucefully");
+  });
+};
+module.exports = connectToMongo;
---
+++ my-app/backend/index.js
@@ -0,0 +1,14 @@
+const connectToMongo = require("./db");
+const express = require("express");
+
+connectToMongo();
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
