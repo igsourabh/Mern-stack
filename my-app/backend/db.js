e50c853e58fba30871edc43b04bb124b05d6ab6d
igsourabh <sourabhvaish007@gmail.com>
2021-12-25 13:39:40 +0530

    server api working

--- my-app/backend/db.js
+++ my-app/backend/db.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
-const mongoURI =
-  "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
+const mongoURI ="mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
 
 const connectToMongo = () => {
   mongoose.connect(mongoURI, () => {
--- my-app/backend/index.js
+++ my-app/backend/index.js
@@ -1,7 +1,8 @@
 const connectToMongo = require("./db");
-const express = require("express");
 
+const express = require("express");
 connectToMongo();
+
 const app = express();
 const port = 3000;
 
