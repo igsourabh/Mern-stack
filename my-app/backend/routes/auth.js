00223988e5fb15f13fefb2ccdbf447cb9c02984b
igsourabh <sourabhvaish007@gmail.com>
2021-12-29 21:14:04 +0530

    refactor code with adding important comment

--- my-app/backend/routes/auth.js
+++ my-app/backend/routes/auth.js
@@ -6,27 +6,41 @@ const { body, validationResult } = require("express-validator");
 // Create a User using: POST "/api/auth/". Doesn't require Auth
 router.post(
   "/createUser",
+  // this is the end point for auth .js
   [
     body("name").isLength({ min: 3 }),
     body("email").isEmail(),
     body("password").isLength({ min: 5 }),
   ],
+
   async (req, res) => {
+    // using express validatr
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    let user = await User.findOne({ email: req.body.email });
-    if (user) {
-      return res.status(400).json({ error: "user of this emails has bee alreay exist" });
+    // wrap this code in try cath method
+    try {
+      // using async await mather rather then express validtor defalut promises
+      let user = await User.findOne({ email: req.body.email });
+      // if user has ame emale in database then is programe not run it will stpe here
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "user of this emails has bee alreay exist" });
+      }
+      user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      });
+
+      res.json(user);
+    } catch (error) {
+      // if some error occured catch will give some console error
+      console.error(error.message);
+      res.status(500).send("some error occured");
     }
-    user = await User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    });
-  
-  res.json({"NIce": "nice"})
   }
 );
 
