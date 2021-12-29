fff3912bf75895bb410aae7d8342470b17eed444
igsourabh <sourabhvaish007@gmail.com>
2021-12-29 18:44:11 +0530

    improve some code
    refactor code and if i delete mongodb collection then my database automatically recreate when i hit post request

--- my-app/backend/routes/auth.js
+++ my-app/backend/routes/auth.js
@@ -5,22 +5,28 @@ const { body, validationResult } = require("express-validator");
 
 // Create a User using: POST "/api/auth/". Doesn't require Auth
 router.post(
-  "/",
+  "/createUser",
   [
     body("name").isLength({ min: 3 }),
     body("email").isEmail(),
     body("password").isLength({ min: 5 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
+    let user = await User.findOne({ email: req.body.email });
+    if (user) {
+      return res.status(400).json({ error: "user of this emails has bee alreay exist" });
+    }
+    user = await User.create({
       name: req.body.name,
       email: req.body.email,
       password: req.body.password,
-    }).then((user) => res.json(user)).catch((err)=>{res.json({email:"eneter unique for email"})})
+    });
+  
+  res.json({"NIce": "nice"})
   }
 );
 
